b38c1b9a64f3f4cac73f243306cd572785989047
Nikhil Gupta <connectnikhil.ind@gmail.com>
2021-09-09 12:12:33 +0530

    safety check

--- app/src/main/java/com/jcodeNikhil/jmusic/Player.java
+++ app/src/main/java/com/jcodeNikhil/jmusic/Player.java
@@ -2,13 +2,36 @@ package com.jcodeNikhil.jmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class Player extends AppCompatActivity {
 
+    TextView audioTitle;
+    ImageView play, previous, next;
+    ArrayList<File> songs;
+    MediaPlayer mediaPlayer;
+    String textContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        audioTitle = findViewById(R.id.audio_title);
+        play = findViewById(R.id.play);
+        previous = findViewById(R.id.previous);
+        next = findViewById(R.id.next);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        songs = (ArrayList) bundle.getParcelableArrayList("songList");
+        textContent = intent.getStringExtra("currentSong");
+        audioTitle.setText(textContent);
     }
 }
