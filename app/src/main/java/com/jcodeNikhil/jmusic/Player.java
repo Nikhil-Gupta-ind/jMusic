92e3c283c1dda312b44f5c74fd074857b38e9f55
Nikhil Gupta <connectnikhil.ind@gmail.com>
2021-09-09 13:14:39 +0530

    mediaplayer started

--- app/src/main/java/com/jcodeNikhil/jmusic/Player.java
+++ app/src/main/java/com/jcodeNikhil/jmusic/Player.java
@@ -4,26 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.File;
 import java.util.ArrayList;
 
 public class Player extends AppCompatActivity {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
 
     TextView audioTitle;
+    SeekBar seekBar;
+    Thread updateSeek;
     ImageView play, previous, next;
     ArrayList<File> songs;
     MediaPlayer mediaPlayer;
     String textContent;
+    int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
         audioTitle = findViewById(R.id.audio_title);
+        seekBar = findViewById(R.id.seekBar);
         play = findViewById(R.id.play);
         previous = findViewById(R.id.previous);
         next = findViewById(R.id.next);
@@ -33,5 +45,12 @@ public class Player extends AppCompatActivity {
         songs = (ArrayList) bundle.getParcelableArrayList("songList");
         textContent = intent.getStringExtra("currentSong");
         audioTitle.setText(textContent);
+
+        position = intent.getIntExtra("position", 0);
+        Uri uri = Uri.parse(songs.get(position).toString());
+        mediaPlayer = MediaPlayer.create(this, uri);
+        mediaPlayer.start();
+
+        //ASA player starts set audio title,
     }
 }
