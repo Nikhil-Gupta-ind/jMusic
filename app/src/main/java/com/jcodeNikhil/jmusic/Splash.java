3c231315ab79d5d87ca0c4ebce3b7a9431ca211b
Nikhil Gupta <85886260+jcodeNikhil@users.noreply.github.com>
2021-10-08 22:02:16 +0530

    new update

--- app/src/main/java/com/jcodeNikhil/jmusic/Splash.java
+++ app/src/main/java/com/jcodeNikhil/jmusic/Splash.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
 
+    Animation animation;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        imageView = findViewById(R.id.imageView);
+        animation = AnimationUtils.loadAnimation(this,R.anim.logo_anim);
+        imageView.setAnimation(animation);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
