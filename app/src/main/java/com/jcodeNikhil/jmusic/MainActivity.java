81ba6e31a86a8ab65f4629b44ede15753594d012
Nikhil Gupta <85886260+jcodeNikhil@users.noreply.github.com>
2021-11-24 14:27:05 +0530

    File loader added

--- app/src/main/java/com/jcodeNikhil/jmusic/MainActivity.java
+++ app/src/main/java/com/jcodeNikhil/jmusic/MainActivity.java
@@ -11,13 +11,18 @@ package com.jcodeNikhil.jmusic;
 //    // TODO (9) Create ui/ux also show album art
 //    // TODO (9) Create album art clickable
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.AsyncTaskLoader;
+import androidx.loader.content.Loader;
 
 import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,6 +33,7 @@ import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,13 +47,21 @@ import com.karumi.dexter.listener.single.PermissionListener;
 import java.io.File;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<File>> {
 
     ListView listView;
-    TextView textView;
+    TextView textView,empty;
     Animation animation;
     FrameLayout frameLayout;
     ImageView playAll;
+    /*
+     * This number will uniquely identify our Loader and is chosen arbitrarily. You can change this
+     * to any number you like, as long as you use the same variable name.
+     */
+    private static final int FILE_LOADER = 22;
+    ArrayList<File> mySongs;
+    ArrayAdapter<String> adapter;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         textView = findViewById(R.id.textView);
         playAll = findViewById(R.id.play_all);
+        progressBar = findViewById(R.id.progress_circular);
+        empty = findViewById(R.id.empty);
 //        Resources res = getResources();
 //        String text = res.getString(R.string.welcome_messages, "h", 1);
         String format = "%1$-14s";
@@ -67,37 +83,19 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
 //                        Toast.makeText(MainActivity.this, "Storage permission granted", Toast.LENGTH_SHORT).show();
-
+                        /*perhaps useful instead saveInstance, but it's not working here, something is causing the main activity to destroy and return to splash
+                        getSupportLoaderManager().initLoader(FILE_LOADER, null, MainActivity.this);*/
                         animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.listanim);
                         frameLayout.setAnimation(animation);
 
-                        ArrayList<File> mySongs = fetchSongs(Environment.getExternalStorageDirectory());
-                        String [] items = new String[mySongs.size()];
-                        for (int i=0; i<mySongs.size();i++){
-                            items [i] = mySongs.get(i).getName().replace(".mp3", "");
+                        //can be used for reloading list
+                        LoaderManager loaderManager = getSupportLoaderManager();
+                        Loader<ArrayList<File>> fileLoader = loaderManager.getLoader(FILE_LOADER);
+                        if (fileLoader == null){
+                            loaderManager.initLoader(FILE_LOADER,null,MainActivity.this);
+                        } else {
+                            loaderManager.restartLoader(FILE_LOADER, null, MainActivity.this);
                         }
-                        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, items);
-                        listView.setAdapter(adapter);
-                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                Intent intent = new Intent(MainActivity.this, Player.class);
-                                intent.putExtra("songList", mySongs);
-                                intent.putExtra("position", position);
-                                startActivity(intent);
-                            }
-                        });
-                        playAll.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                Intent intent = new Intent(MainActivity.this, Player.class);
-                                String currentSong = listView.getItemAtPosition(0).toString();
-                                intent.putExtra("songList", mySongs);
-                                intent.putExtra("currentSong", currentSong);
-                                intent.putExtra("position", 0);
-                                startActivity(intent);
-                            }
-                        });
                     }
 
                     @Override
@@ -123,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
                     arrayList.addAll(fetchSongs(myFile));
                 }
                 else {
-                    if(myFile.getName().endsWith(".mp3") || myFile.getName().endsWith(".m4a") && !myFile.getName().startsWith(".")){
+                    if(myFile.getName().endsWith(".mp3") && !myFile.getName().startsWith(".")){
                         arrayList.add(myFile);
                     }
                 }
@@ -131,6 +129,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return arrayList;
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -167,5 +166,72 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         finishAffinity();
+    }
+
+    @NonNull
+    @Override
+    public Loader<ArrayList<File>> onCreateLoader(int id, @Nullable Bundle args) {
+        return new AsyncTaskLoader<ArrayList<File>>(this) {
+            @Override
+            protected void onStartLoading() {
+                progressBar.setVisibility(View.VISIBLE);
+                forceLoad();
+            }
+
+            @Nullable
+            @Override
+            public ArrayList<File> loadInBackground() {
+                mySongs = fetchSongs(Environment.getExternalStorageDirectory());
+                return mySongs;
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<ArrayList<File>> loader, ArrayList<File> data) {
+        progressBar.setVisibility(View.INVISIBLE);
+        if (null == data) {
+//            showErrorMessage();
+            Log.d("Control", "onLoadFinished: null");
+            empty.setVisibility(View.VISIBLE);
+        } else {
+            /*mSearchResultsTextView.setText(data);
+            showJsonDataView();*/
+            Log.d("Control", "onLoadFinished: not null");
+            String [] items = new String[mySongs.size()];
+            for (int i=0; i<mySongs.size();i++){
+                items [i] = mySongs.get(i).getName().replace(".mp3", "");
+            }
+            adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, items);
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent(MainActivity.this, Player.class);
+                    intent.putExtra("songList", mySongs);
+                    intent.putExtra("position", position);
+                    startActivity(intent);
+                }
+            });
+            playAll.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, Player.class);
+                    String currentSong = listView.getItemAtPosition(0).toString();
+                    intent.putExtra("songList", mySongs);
+                    intent.putExtra("currentSong", currentSong);
+                    intent.putExtra("position", 0);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<ArrayList<File>> loader) {
+        /*
+         * We aren't using this method in our example application, but we are required to Override
+         * it to implement the LoaderCallbacks<String> interface
+         */
     }
 }
